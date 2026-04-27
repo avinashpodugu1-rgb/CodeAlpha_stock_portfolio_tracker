b20793b6a4e729b92ff5da9af92507812df46060
avinashpodugu1-rgb <avinashpodugu1@gmail.com>
2026-04-27 19:47:49 +0530

    Add files via upload

---
+++ Stock_Portfolio_Tracker.py
@@ -0,0 +1,36 @@
+stocks = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOG": 140,
+    "MSFT": 320
+}
+
+total = 0
+portfolio = []
+
+print("📈 Stock Portfolio Tracker")
+
+while True:
+    name = input("Enter stock name (or done): ").upper()
+    if name == "DONE":
+        break
+
+    if name in stocks:
+        qty = int(input("Enter quantity: "))
+        value = stocks[name] * qty
+        total += value
+        portfolio.append(f"{name} x {qty} = ${value}")
+    else:
+        print("Stock not found!")
+
+print("\nYour Portfolio:")
+for item in portfolio:
+    print(item)
+
+print("💰 Total Investment Value = $", total)
+
+with open("portfolio.txt", "w") as file:
+    file.write("\n".join(portfolio))
+    file.write(f"\nTotal = ${total}")
+
+print("Saved to portfolio.txt")
